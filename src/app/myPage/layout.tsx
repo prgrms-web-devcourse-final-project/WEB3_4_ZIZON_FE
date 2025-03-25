2cd7287942760c8ca09adc90241a820cf664879a
kimyougmin <dudals896@gmail.com>
2025-03-25 22:40:09 +0900

    fix: 도커 빌더 오류 해결
    https://www.notion.so/FE-CI-CD-1c14873f28dd8035b28df461ffb46318?pvs=4
    위 링크에 상세히 정리되어 있습니다.

---
+++ src/app/myPage/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import '../globals.css';
+import ReactQueryClientProvider from '@/config/ReactQueryClientProvider';
+
+export const metadata: Metadata = {
+  title: '💫DopDang',
+  description: 'Serve you the professional touch',
+};
+
+export default function MyPageLayout({
+                                     children,
+                                   }: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ReactQueryClientProvider>
+      <html lang="en">
+      <body>{children}</body>
+      </html>
+    </ReactQueryClientProvider>
+  );
+}
---
+++ src/app/store/products/[product_id]/page.tsx
@@ -0,0 +1,3 @@
+export default function ProductIdPage() {
+  return <div></div>
+}
