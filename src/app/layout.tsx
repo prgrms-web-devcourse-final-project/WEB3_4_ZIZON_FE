471b09cbeb27395ed28f9a022bb988c1161cb705
jjeongsu <jeongsu.dev@gmail.com>
2025-03-27 10:13:24 +0900

    chore : 전역에 pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,20 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import ReactQueryClientProvider from '@/config/ReactQueryClientProvider';
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: '💫DopDang',
   description: 'Serve you the professional touch',
 };
+
+const pretendard = localFont({
+  src: '../../public/font/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '400 700',
+  variable: '--font-pretendard',
+  fallback: ['Arial', 'sans-serif'],
+});
 
 export default function RootLayout({
   children,
@@ -14,8 +23,8 @@ export default function RootLayout({
 }>) {
   return (
     <ReactQueryClientProvider>
-      <html lang="en">
-        <body>{children}</body>
+      <html lang="en" className={`${pretendard.variable}`}>
+        <body className={`${pretendard.className}`}>{children}</body>
       </html>
     </ReactQueryClientProvider>
   );
