339af175ec7d6d408af7f4d1ddaf3c9a05ddf261
Sean <joajoa70584@gmail.com>
2025-03-28 17:40:56 +0900

    feat: StarTag 컴포넌트 구현 (#29)

---
+++ src/components/atoms/starTag/StarTag.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import StarOnLarge from 'public/icons/StarOnLarge.svg';
+
+interface StarTagProps {
+  rating: number;
+  reviewCount: number;
+}
+
+const StarTag: React.FC<StarTagProps> = ({ rating, reviewCount }) => {
+  return (
+    <div className="flex items-center gap-2 px-12 py-8 bg-black1 border border-black3 rounded-full">
+      <Image src={StarOnLarge} alt="별점" width={16} height={14} />
+      <div className="flex gap-2 items-end">
+        <span className="inline-block text-13 mr-2 text-black7 font-medium">
+          {rating.toFixed(1)}
+        </span>
+        <span className="inline-block text-11 text-black7 font-regular">({reviewCount})</span>
+      </div>
+    </div>
+  );
+};
+
+export default StarTag;
