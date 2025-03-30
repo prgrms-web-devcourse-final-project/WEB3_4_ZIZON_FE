1f7efbf4916e7ad77977df34d31ecd9eca791abb
jjeongsu <jeongsu.dev@gmail.com>
2025-03-30 19:19:00 +0900

    test : SearchBar 컴포넌트의 테스트 코드 추가

---
+++ src/components/atoms/searchBar/SearchBar.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SearchBar from './SearchBar';
+import React from 'react';
+
+const meta = {
+  title: 'Atoms/SearchBar',
+  component: SearchBar,
+  parameters: {
+    layout: 'centered',
+    componentSubtitle: 'type에 따라 너비가 고정된 검색창 컴포넌트',
+    docs: {
+      description: {
+        component:
+          '**default** 기본 검색창 \n\n**rounded** 채팅페이지에서 사용되는 검색창 \n\n**large** 메인페이지에서 사용되는 검색창',
+      },
+    },
+  },
+  tags: ['autodocs'],
+} as Meta<typeof SearchBar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    type: 'default',
+    placedholder: '검색어를 입력해주세요',
+    onChange: (value: string) => console.log(value),
+    value: '',
+  },
+};
+
+export const Rounded: Story = {
+  args: {
+    type: 'rounded',
+    placedholder: '검색어를 입력해주세요',
+    onChange: (value: string) => console.log(value),
+    value: '',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    type: 'large',
+    placedholder: '검색어를 입력해주세요',
+    onChange: (value: string) => console.log(value),
+    value: '',
+  },
+};
+
+export const Edit = () => {
+  const [value, setValue] = React.useState<string>('');
+
+  return (
+    <SearchBar
+      type="default"
+      placedholder="검색어를 입력해주세요"
+      onChange={(value: string) => setValue(value)}
+      value={value}
+    />
+  );
+};
