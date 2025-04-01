7de7ae99e5e19799be699fef1961a35b2e6517f3
jjeongsu <jeongsu.dev@gmail.com>
2025-04-01 12:09:51 +0900

    feat : ChatListItem 컴포넌트 구현(#44)

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -18,7 +18,7 @@ export const getTimeAgo = (date: Date): string => {
 };
 
 export const getDotSeparatedDate = (date: Date) => {
-  const year = date.getFullYear();
+  const year = String(date.getFullYear()).slice(-2);
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
   return `${year}.${month}.${day}`;
