bdebc3425b4b54dfadf7aa0492868a882e7490f7
jjeongsu <jeongsu.dev@gmail.com>
2025-04-01 23:49:35 +0900

    test : RequestOfferBox의 테스트코드 추가(#50)

---
+++ src/components/molecules/requestOfferBox/RequestOfferBox.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import RequestOfferBox from './RequestOfferBox';
+
+const meta = {
+  title: 'Molecules/RequestOfferBox',
+  component: RequestOfferBox,
+  parameters: {
+    layout: 'centered',
+    componentSubtitle: '메인페이지에서 사용되는 최근 등록된 서비스 컴포넌트',
+  },
+  tags: ['autodocs'],
+} as Meta<typeof RequestOfferBox>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    name: '이수정',
+    expertId: 'expertId',
+  },
+};
