725cc23a4b7b8bf440c17a990b7714ee5c4455b9
jjeongsu <jeongsu.dev@gmail.com>
2025-04-01 12:48:13 +0900

    feat : Banner 컴포넌트 구현 (#50)

---
+++ src/components/atoms/banner/Banner.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+interface BannerProps {
+  title: string;
+  subTitle: string;
+}
+
+export default function Banner({ title, subTitle }: BannerProps) {
+  return (
+    <div className="relative w-1280 h-300">
+      <Image
+        src="public/images/ExpertBanner.png"
+        width={1280}
+        height={300}
+        alt="expert-banner-image w-full h-full"
+      />
+      <div className="w-fit h-fit absolute top-80 left-72">
+        <h2 className="text-black1 font-bold text-32 mb-16 whitespace-pre-line leading-[1.5]">
+          {title}
+        </h2>
+        <h3 className="text-black1 font-regular text-16">{subTitle}</h3>
+      </div>
+    </div>
+  );
+}
