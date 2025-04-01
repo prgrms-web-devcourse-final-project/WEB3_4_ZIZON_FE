eead50a5fc0d4e8c36276d74df0b2d9ad98c0f98
kimyougmin <dudals896@gmail.com>
2025-04-01 14:41:51 +0900

    feat:commissionTopBox 제작

---
+++ src/components/molecules/commissionTopBox/CommissionTopBox.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ProgressBar from '@/components/atoms/progressBar/ProgressBar';
+import StandardButton from '@/components/atoms/standardButton/StandardButton';
+
+interface CommissionTopBox {
+  title: string;
+  progressStep: number;
+  isBefore: boolean;
+  onClickBefore: () => void;
+}
+
+function CommissionTopBox({title, progressStep, isBefore, onClickBefore,}:CommissionTopBox) {
+  return (
+    <div className="grid grid-cols-5 bg-black1 px-40 py-24 rounded-lg justify-between">
+      <div className="col-span-3">
+        <p className="mb-12 text-20 font-medium">{title}</p>
+        <ProgressBar step={progressStep} />
+      </div>
+      <div></div>
+      <div className="place-items-end">
+        {isBefore? <StandardButton text={"이전단계"} size={"full"} onClick={onClickBefore} disabled={false}/> : null}
+      </div>
+    </div>
+  );
+}
+
+export default CommissionTopBox;
