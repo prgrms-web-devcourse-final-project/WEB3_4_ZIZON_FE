08c9eea366c6ec12e4c3669a446ee0b8ba2802b5
jjeongsu <jeongsu.dev@gmail.com>
2025-04-01 17:41:34 +0900

    test : ExpertFilterTab의 테스트 코드 작성(#50)

---
+++ src/components/molecules/expertFilterTab/ExpertFilterTab.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import ExpertFilterTab from './ExpertFilterTab';
+import { useState } from 'react';
+import { CareerCategoryIdType, ProjectCategoryIdType } from '@/constants/category';
+const meta = {
+  title: 'Molecules/ExpertFilterTab',
+  component: ExpertFilterTab,
+  parameters: {
+    layout: 'centered',
+    componentSubtitle: '전문가 조회 페이지의 사이드바에 사용되는 필터 탭',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof ExpertFilterTab>;
+
+export default meta;
+type Story = StoryObj<typeof ExpertFilterTab>;
+
+export const Default = () => {
+  const [selectedCategory, setSelectedCategory] = useState<ProjectCategoryIdType>(1000);
+  const [selectedCareer, setSelectedCareer] = useState<CareerCategoryIdType>('junior');
+
+  return (
+    <ExpertFilterTab
+      selectedCategory={selectedCategory}
+      selectedCareer={selectedCareer}
+      onCategoryChange={(categoryId: ProjectCategoryIdType, checked: boolean) =>
+        setSelectedCategory(categoryId)
+      }
+      onCareerChange={(careerId: CareerCategoryIdType) => setSelectedCareer(careerId)}
+    />
+  );
+};
