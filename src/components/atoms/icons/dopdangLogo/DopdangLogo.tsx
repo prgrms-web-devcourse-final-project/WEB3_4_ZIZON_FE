692dde49c0e21b3402310df55b1c4b45d97cb829
Sean <joajoa70584@gmail.com>
2025-04-02 11:38:15 +0900

    feat: Dopdang 로고 영어 타입 추가 (#52)

--- src/components/atoms/icons/dopdangLogo/DopdangLogo.tsx
+++ src/components/atoms/icons/dopdangLogo/DopdangLogo.tsx
@@ -1,11 +1,23 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-export default function DopdangLogo() {
+type DopdangLogoProps = {
+  type: 'kr' | 'en';
+};
+
+export default function DopdangLogo({ type = 'kr' }: DopdangLogoProps) {
+  const width = type === 'kr' ? 61 : 221;
+  const height = type === 'kr' ? 32 : 36;
+
   return (
-    <h1 className="w-61 h-32">
+    <h1 className={`w-${width} h-${height}`}>
       <Link href="/" className="block">
-        <Image src="/logo/dopdang-logo-kr.svg" alt="dopdang-logo" width={61} height={32} />
+        <Image
+          src={`/logo/dopdang-logo-${type}.svg`}
+          alt="dopdang-logo"
+          width={width}
+          height={height}
+        />
       </Link>
     </h1>
   );
