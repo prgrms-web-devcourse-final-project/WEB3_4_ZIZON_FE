e1d07e5b6723aea8d7de62050c913ef4879562d3
Sean <joajoa70584@gmail.com>
2025-04-02 09:44:33 +0900

    feat: ExpertCategory 상수 파일 추가 (#58)

---
+++ src/constants/expert.ts
@@ -0,0 +1,9 @@
+export enum ExpertCategory {
+  ALL = '전체',
+  MOVING = '이사/청소',
+  REPAIR = '설치/수리',
+  TUTORING = '과외',
+  HOBBY = '취미/자기계발',
+}
+
+export const EXPERT_CATEGORIES = Object.values(ExpertCategory);
