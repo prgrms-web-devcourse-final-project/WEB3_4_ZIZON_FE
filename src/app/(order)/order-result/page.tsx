2ffd5346b9df402a40c1bbd6cf0d08e0c7f99fb3
jjeongsu <jeongsu.dev@gmail.com>
2025-04-02 15:25:42 +0900

    feat : 주문 결과 페이지 퍼블리싱(#62)

--- src/app/(order)/order-result/page.tsx
+++ src/app/(order)/order-result/page.tsx
@@ -1,3 +1,27 @@
+import OrderResult from '@/components/organisms/order/orderResult/OrderResult';
+import OrderResultTemplate from '@/components/templates/orderResultTemplate/OrderResultTemplate';
+
 export default function OrderResultPage() {
-  return <div>결제결과 페이지</div>;
+  // 여기서 결제 완료 정보 호출
+
+  return (
+    // === OrderResultTemplate 사용 예시 ===
+    <OrderResultTemplate
+      OrderResultComponent={
+        <OrderResult
+          infoList={[
+            {
+              attribute: '구매 상품',
+              value: '이사 서비스',
+            },
+            {
+              attribute: '전문가',
+              value: '이상훈',
+            },
+          ]}
+          totalPrice={1000000}
+        />
+      }
+    />
+  );
 }
