1e0169406431063a1327411b75f03d365c17bb7b
jjeongsu <jeongsu.dev@gmail.com>
2025-04-02 23:38:23 +0900

    refactor: 결제페이지 SSR로 리팩토링(#68)

--- src/app/(order)/order/page.tsx
+++ src/app/(order)/order/page.tsx
@@ -1,19 +1,17 @@
-'use client';
-import { loadTossPayments } from '@tosspayments/payment-sdk';
 import ChargeInfo from '@/components/molecules/order/chargeInfo/ChargeInfo';
 import OrderInfoList from '@/components/organisms/order/orderInfoList/OrderInfoList';
 import OrderTemplate from '@/components/templates/orderTemplate/OrderTemplate';
-import { PaymentType } from '@/types/payment';
-import { useSearchParams } from 'next/navigation';
+import PaymentButton from '@/components/atoms/buttons/PaymentButton';
 
-export default function OrderPage() {
-  const searchParams = useSearchParams();
-
-  const id = searchParams.get('id');
-  const paymentType = searchParams.get('type') as PaymentType; // PROJECT, ORDER, PROVISION
+export default async function OrderPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ type: string; id: string }>;
+}) {
+  const { type: paymentType, id } = await searchParams;
 
   // 1. 서버에서 결제 정보(orderId, customerKey, 구매 상품 정보)를 가져오기 -> RQ로 래핑
-  const orderRes = await fetch('/payments/orderId', {
+  const orderRes = await fetch(`${process.env.SERVER_URL}/payments/orderId`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -25,39 +23,15 @@ export default function OrderPage() {
   }).then(res => res.json());
 
   // 2. 결제에 필요한 orderId, customerKey
-  const { orderId, customerKey } = orderRes;
+  const { orderId, customerKey, orderInfo } = orderRes;
   // TODO : 서버에서 orderRes에서 구매상품정보 가져오는 기능 추가예정
-
-  // 3. 결제하기 버튼 클릭 시 처리할 이벤트 핸들러
-  const handlePayment = async () => {
-    if (!process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY) {
-      throw new Error('NEXT_PUBLIC_TOSS_KEY is not defined');
-    }
-
-    // TRY : loadTossPayments(customerKey)
-    loadTossPayments(process.env.NEXT_PUBLIC_TOSS_CLIENT_KEY)
-      .then(tossPayments => {
-        tossPayments.requestPayment('카드', {
-          orderId: orderId, // 주문번호
-          amount: 100, // 결제 금액
-          orderName: '테스트 결제', // 구매상품
-          customerName: '김토스', // 구매자 이름
-          successUrl: `${process.env.SERVER_URL}/payments/success`, // 결제 성공 시  URL (server)
-          failUrl: `${process.env.SERVER_URL}/payments/fail`, // 결제 실패 시 URL (server)
-        });
-      })
-      .catch(function (error) {
-        if (error.code === 'USER_CANCEL') {
-          // 구매자가 결제창을 닫았을 때 에러 처리
-        } else if (error.code === 'INVALID_CARD_COMPANY') {
-          // 유효하지 않은 카드 코드에 대한 에러 처리
-        }
-      });
-  };
 
   return (
     // == Template 사용 예 ===
-    <OrderTemplate paymentType={'PROJECT'} onPaymentClick={handlePayment}>
+    <OrderTemplate
+      paymentType={'PROJECT'}
+      paymentButton={<PaymentButton orderId={orderId} orderInfo={orderInfo} />}
+    >
       <OrderInfoList
         title="구매상품"
         infoList={[
