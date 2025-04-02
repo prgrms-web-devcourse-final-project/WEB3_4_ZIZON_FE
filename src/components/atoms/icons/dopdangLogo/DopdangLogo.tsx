0d7ff57c357c1e9f3f520c9044f4c4b1410b695a
Sean <joajoa70584@gmail.com>
2025-04-02 16:35:36 +0900

    feat: Dopdang 로고 signup 타입 추가 (#52)

--- src/components/atoms/icons/dopdangLogo/DopdangLogo.tsx
+++ src/components/atoms/icons/dopdangLogo/DopdangLogo.tsx
@@ -2,22 +2,40 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 type DopdangLogoProps = {
-  type: 'kr' | 'en';
+  type: 'kr' | 'en' | 'signup';
 };
 
-export default function DopdangLogo({ type = 'kr' }: DopdangLogoProps) {
-  const width = type === 'kr' ? 61 : 221;
-  const height = type === 'kr' ? 32 : 36;
+const logoStyles: Record<
+  DopdangLogoProps['type'],
+  {
+    src: string;
+    width: number;
+    height: number;
+  }
+> = {
+  kr: {
+    src: '/logo/dopdang-logo-kr.svg',
+    width: 61,
+    height: 32,
+  },
+  en: {
+    src: '/logo/dopdang-logo-en.svg',
+    width: 221,
+    height: 36,
+  },
+  signup: {
+    src: '/logo/dopdang-logo-signup.svg',
+    width: 180,
+    height: 36,
+  },
+} as const;
 
+export default function DopdangLogo({ type = 'kr' }: DopdangLogoProps) {
+  const { src, width, height } = logoStyles[type];
   return (
     <h1 className={`w-${width} h-${height}`}>
       <Link href="/" className="block">
-        <Image
-          src={`/logo/dopdang-logo-${type}.svg`}
-          alt="dopdang-logo"
-          width={width}
-          height={height}
-        />
+        <Image src={src} alt="dopdang-logo" width={width} height={height} />
       </Link>
     </h1>
   );
