007adf4eafa7495d33d55bec4fe91f1e6d84f3fa
jjeongsu <jeongsu.dev@gmail.com>
2025-04-02 22:50:07 +0900

    feat : 토스페이먼트 결제로직 추가 (#68)

--- src/app/(order)/order-result/page.tsx
+++ src/app/(order)/order-result/page.tsx
@@ -1,8 +1,12 @@
 import OrderResult from '@/components/organisms/order/orderResult/OrderResult';
 import OrderResultTemplate from '@/components/templates/orderResultTemplate/OrderResultTemplate';
 
-export default function OrderResultPage() {
-  // 여기서 결제 완료 정보 호출
+export default async function OrderResultPage() {
+  // 성공/실패시 자동으로 리디렉션되는 페이지
+  // const secretKey = process.env.TOSS_SECRET_KEY || '';
+  // const basTOKEN = Buffer.from(`${secretKey}`, 'utf-8').toString('base64');
+
+  // const url = 'https://api.tosspayments.com/v1/payments/orders/';
 
   return (
     // === OrderResultTemplate 사용 예시 ===
