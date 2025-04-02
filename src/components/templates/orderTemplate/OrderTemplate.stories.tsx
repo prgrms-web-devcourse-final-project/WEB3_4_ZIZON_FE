dc7b0cf4228716f7f0149277dc41fd4cf3a2ccc2
jjeongsu <jeongsu.dev@gmail.com>
2025-04-02 14:31:56 +0900

    test : OrderTemplate의 테스트코드 작성(#62)

---
+++ src/components/templates/orderTemplate/OrderTemplate.stories.tsx
@@ -0,0 +1,53 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import OrderTemplate from './OrderTemplate';
+import OrderInfoList from '@/components/organisms/orderInfoList/OrderInfoList';
+import ChargeInfo from '@/components/molecules/order/chargeInfo/ChargeInfo';
+
+const meta = {
+  title: 'Templates/OrderTemplate',
+  component: OrderTemplate,
+  parameters: {
+    layout: 'fullscreen',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof OrderTemplate>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default = () => {
+  const handlePayment = () => {
+    // 결제하기 버튼 클릭 시 처리할 로직
+  };
+  return (
+    <OrderTemplate paymentType={'PROJECT'} onPaymentClick={handlePayment}>
+      <OrderInfoList
+        title="구매상품"
+        infoList={[
+          {
+            attribute: '구매 상품',
+            value: '이사 서비스',
+          },
+          {
+            attribute: '전문가',
+            value: '이상훈',
+          },
+        ]}
+      />
+      <OrderInfoList
+        title="견적상세"
+        infoList={[
+          {
+            attribute: '이사 날짜',
+            value: '2023년 10월 1일',
+          },
+          {
+            attribute: '이사 시간',
+            value: '오후 2시',
+          },
+        ]}
+      />
+      <ChargeInfo serviceFee={10000} totalPrice={8000} />
+    </OrderTemplate>
+  );
+};
