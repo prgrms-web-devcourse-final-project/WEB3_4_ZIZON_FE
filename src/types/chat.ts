a3cba4f785f12ac1fb62bca5a8b20343e05c68af
jjeongsu <jeongsu.dev@gmail.com>
2025-04-03 18:27:48 +0900

    refactor : 채팅방 타입 정보 변경

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,11 +1,9 @@
+// 채팅방 목록에 보여지는 채팅방 타입
 export interface ChattingRoomType {
-  id: string; // 채팅방Id 를 쿼리 파라미터에 전달
-  client_id: string;
-  expert_id: string;
-  created_at: string;
-  lasst_message: string;
-  last_message_time: string;
-  // 백엔드에 추가 요청한 필드
-  expert_name: string;
-  expert_profile_image: string;
+  roomId: string;
+  otherUserName: string;
+  otherUserProfile: string;
+  lastMessage: string;
+  lastMessageTime: string;
+  unreadCount: number;
 }
