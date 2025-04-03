38e381ac75b524b24293bdec3926e8047e596aa6
jjeongsu <jeongsu.dev@gmail.com>
2025-04-03 11:51:48 +0900

    feat : ChattingInputBox 컴포넌트 구현(#63)

---
+++ src/components/molecules/chat/ChattingInputBox.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import StandardButton from '@/components/atoms/buttons/standardButton/StandardButton';
+import FileInput from '@/components/atoms/inputs/fileInput/FileInput';
+import TextInput from '@/components/atoms/inputs/textInput/TextInput';
+import { useState } from 'react';
+
+export default function ChattingInputBox() {
+  const [message, setMessage] = useState<string>('');
+  const [file, setFile] = useState<File | null>(null);
+  return (
+    <div className="w-full flex flex-col gap-10">
+      <TextInput
+        value={message}
+        onChange={(value: string) => setMessage(value)}
+        placeholder="메시지를 입력해주세요. (Enter: 줄바꿈 / Ctrl+Enter: 전송)"
+        id="chatting-input"
+        type="text"
+        disabled={false}
+        color="white"
+      />
+
+      {/* 채팅 전송 */}
+      <div className="flex justify-between ">
+        <FileInput
+          label="첨부파일"
+          name="file-input"
+          accept="image/*, .pdf, .docx"
+          onChange={(file: File) => {
+            setFile(file);
+            console.log('첨부파일:', file);
+          }}
+        />
+        <StandardButton
+          text="전송"
+          state="dark"
+          disabled={false}
+          onClick={() => {
+            // 메시지 전송 로직
+            console.log('메시지 전송:', message);
+            setMessage(''); // 전송 후 입력창 초기화
+          }}
+        />
+      </div>
+    </div>
+  );
+}
