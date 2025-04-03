e294e91147ec5b2628bfab60a5f801390e4616e7
kimyougmin <dudals896@gmail.com>
2025-04-03 15:29:41 +0900

    feat: CommissionTemplate 헤더 제작

--- src/app/commission/page.tsx
+++ src/app/commission/page.tsx
@@ -1,7 +1,15 @@
-import CommonStartStepperTemplate from '@/components/templates/stepperTemplate/common/CommonStartStepperTemplate';
+'use client'
+import React from 'react';
+import CommissionTemplate from '@/components/templates/commissionTemplate/CommissionTemplate';
 
 export default function CommissionPage() {
-  return <div>
-    <CommonStartStepperTemplate />
-  </div>
+  const [searchBar, setSearchBar] = React.useState<string>('')
+  return (
+    <div>
+      <CommissionTemplate
+        value={searchBar}
+        onChange={setSearchBar}
+      />
+    </div>
+  );
 }
---
+++ src/components/templates/commissionTemplate/CommissionTemplate.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import SearchBar from '@/components/atoms/inputs/searchBar/SearchBar';
+
+interface CommissionTemplateProps {
+  value: string;
+  onChange: (value: string) => void;
+}
+
+function CommissionTemplate({value, onChange}: CommissionTemplateProps) {
+  return (
+    <div>
+      <h3 className="text-32 font-semibold">등록된 요청</h3>
+      <SearchBar type={'default'} placedholder={'검색어를 입력해주세요.'} onChange={onChange} value={value}/>
+      <div>
+        <div>
+
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CommissionTemplate;
