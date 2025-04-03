f3a121309b2dbf53b91018dded2d7a145a2e13f7
Sean <joajoa70584@gmail.com>
2025-04-03 23:38:57 +0900

    feat: 마이페이지 레이아웃 및 사이드바 컴포넌트 추가 (#60)
    - 마이페이지 레이아웃에 사용자 역할 및 프로필 정보 처리 로직 추가
    - 전문가가 아닌 사용자의 접근 시 리다이렉트 기능 구현
    - 사이드바 컴포넌트 추가 및 프로필 정보와 사용자 상태 탭 통합
    - ChangePositionButton 및 ProfileInfo 컴포넌트 수정하여 상태 관리 개선
    - StandardButton에 새로운 색상 옵션 추가

--- src/components/atoms/tags/certificationTag/CertificationTag.tsx
+++ src/components/atoms/tags/certificationTag/CertificationTag.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
-import CheckBadge from "public/icons/CheckBadge.svg"
+import CheckBadge from 'public/icons/CheckBadge.svg';
 
 interface CertificationTagProps {
   text: string;
 }
 
-function CertificationTag({text}: CertificationTagProps) {
+function CertificationTag({ text }: CertificationTagProps) {
   return (
     <div className="flex bg-black1 rounded-sm border border-black4 py-4 pl-4 pr-8 max-w-fit">
-      <Image src={CheckBadge} alt={""} width={22} height={22} />
+      <Image src={CheckBadge} alt={''} width={22} height={22} />
       <label className="pl-2 content-center text-13 font-medium">{text} 전문가</label>
     </div>
   );
