4e1e211582ab9e3ae787248e4977fd8803d1a929
jjeongsu <jeongsu.dev@gmail.com>
2025-04-05 15:56:47 +0900

    refactor : 스토어 레이아웃 변경

--- src/components/organisms/store/StoreSearchResgister.tsx
+++ src/components/organisms/store/StoreSearchResgister.tsx
@@ -2,13 +2,11 @@
 
 import StandardButton from '@/components/atoms/buttons/standardButton/StandardButton';
 import SearchBar from '@/components/atoms/inputs/searchBar/SearchBar';
-import useDebounce from '@/hooks/useDebounce';
 import useSetSearchParams from '@/hooks/useSetSearchParams';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 export default function StoreSearchRegister() {
   const [search, setSearch] = useState<string>('');
-  const debouncedValue = useDebounce(search, 200);
   const setSearchParams = useSetSearchParams();
   const router = useRouter();
 
@@ -16,17 +14,17 @@ export default function StoreSearchRegister() {
     router.push('/store/products/register');
   };
 
-  useEffect(() => {
-    if (debouncedValue) {
-      setSearchParams('search', debouncedValue, '/store');
-    }
-  }, [debouncedValue]);
+  const handleSearchChange = (value: string) => {
+    setSearch(value);
+    setSearchParams('search', value, '/store');
+  };
+
   return (
     <div className="w-full flex justify-between max-w-1280">
       <SearchBar
         placeholder="검색어를 입력해주세요"
         type="default"
-        onChange={value => setSearch(value)}
+        onChange={value => handleSearchChange(value)}
         value={search}
       />
 
