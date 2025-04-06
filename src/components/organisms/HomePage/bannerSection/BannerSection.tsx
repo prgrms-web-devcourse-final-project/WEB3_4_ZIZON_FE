3f1c97743555155d70dd7d60f0a5764e98ed06de
Sean <joajoa70584@gmail.com>
2025-04-07 00:08:39 +0900

    feat: 배너 섹션 이미지 포맷 변경 및 SVG 아이콘 추가 (#86)
    - 배너 섹션에서 배경 이미지 포맷을 PNG에서 WEBP로 변경하여 성능 최적화
    - 새로운 Document.svg 아이콘 파일 추가
    - 기존 File.svg 아이콘 파일 삭제
    - next.config.ts에 이미지 포맷 설정 추가

--- src/components/organisms/HomePage/bannerSection/BannerSection.tsx
+++ src/components/organisms/HomePage/bannerSection/BannerSection.tsx
@@ -11,11 +11,12 @@ function BannerSection() {
     <section className="flex flex-col items-center mt-64 relative">
       <div className="absolute top-0 left-1/2 -translate-x-1/2 w-1590 h-390 flex items-center justify-center -z-10">
         <Image
-          src={'/images/main-banner-bg-image.png'}
+          src={'/images/main-banner-bg-image.webp'}
           width={1590}
           height={390}
           alt="banner"
           className="object-cover"
+          priority
         />
       </div>
       <BannerTitle />
