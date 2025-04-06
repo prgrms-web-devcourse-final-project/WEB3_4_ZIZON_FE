338703312b562f30183eb26a08a9b6d1a4d55245
jjeongsu <jeongsu.dev@gmail.com>
2025-04-06 18:04:59 +0900

    chore : 이미지 도메인 추가(#84)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
+  output: 'standalone',
+  images: {
+    domains: [`${process.env.S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com`],
+  },
 };
 
 export default nextConfig;
