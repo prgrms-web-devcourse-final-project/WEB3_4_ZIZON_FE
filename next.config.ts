35b3b935f857ce1e24c7eafc4def3ee6681fba0a
Sean <joajoa70584@gmail.com>
2025-04-07 21:57:18 +0900

    chore: 이미지 도메인 추가 및 설정 업데이트 (#90)
    - next.config.ts 파일에 이미지 도메인 추가하여 외부 이미지 로딩 가능하도록 설정
    - S3 버킷 및 구글, 카카오 CDN 도메인 포함

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
   images: {
+    domains: [
+      'devcouse4-team16-bucket.s3.ap-northeast-2.amazonaws.com',
+      'lh3.googleusercontent.com',
+      'k.kakaocdn.net',
+    ],
     formats: ['image/avif', 'image/webp'],
   },
   async rewrites() {
