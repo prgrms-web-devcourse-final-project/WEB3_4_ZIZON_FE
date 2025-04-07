4bdbb4c2ae9363913932476f95d96d9b241400d9
Sean <joajoa70584@gmail.com>
2025-04-07 23:51:03 +0900

    refactor: 로그인 버튼 스타일 개선 및 disabled 상태 스타일 제거 (#90)
    - LoginButton 컴포넌트에서 disabled 상태에 대한 스타일을 제거하고, 기본 색상만 적용하여 코드 간소화
    - 버튼의 cursor 스타일을 disabled 상태에 맞게 조정하여 사용자 경험 개선

--- src/components/atoms/buttons/loginButton/LoginButton.tsx
+++ src/components/atoms/buttons/loginButton/LoginButton.tsx
@@ -9,15 +9,15 @@ interface LoginButtonProps {
 export default function LoginButton({ type, onClick, disabled }: LoginButtonProps) {
   const styleMap = {
     kakao: {
-      color: 'bg-kakao text-black9 disabled:bg-kakao',
+      color: 'bg-kakao text-black9',
       text: '카카오로 시작하기',
     },
     naver: {
-      color: 'bg-naver text-black1 disabled:bg-naver',
+      color: 'bg-naver text-black1',
       text: '네이버로 시작하기',
     },
     google: {
-      color: 'bg-black1 text-black9 disabled:bg-black1',
+      color: 'bg-black1 text-black9',
       text: '구글로 시작하기',
     },
     default: {
@@ -39,7 +39,7 @@ export default function LoginButton({ type, onClick, disabled }: LoginButtonProp
     <button
       onClick={() => onClick()}
       disabled={disabled}
-      className={`w-404 h-47 py-12 px-16 rounded-[8px] flex gap-12 justify-center items-center  disabled:bg-black6 disabled:text-black9  cursor-pointer ${style.color}`}
+      className={`w-404 h-47 py-12 px-16 rounded-[8px] flex gap-12 justify-center items-center cursor-pointer disabled:opacity-80 ${style.color}`}
     >
       {type === 'kakao' && (
         <Image src="/icons/KakaoLogo.svg" alt="kakao-logo" width={25} height={25} />
