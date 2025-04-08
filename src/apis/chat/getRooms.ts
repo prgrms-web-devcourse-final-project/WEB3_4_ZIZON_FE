311c1dbde2e03ac54c6660bc028b6c5060cfb825
jjeongsu <jeongsu.dev@gmail.com>
2025-04-08 15:11:40 +0900

    feat: 채팅방 왼쪽 사이드바(채팅방목록)API 연동 및 구현(#97)

---
+++ src/apis/chat/getRooms.ts
@@ -0,0 +1,39 @@
+import { APIBuilder } from '@/utils/APIBuilder';
+import { decodeToken } from '@/utils/decodeToken';
+import { cookies } from 'next/headers';
+
+interface RoomType {
+  roomId: 'test3@test.com:한민@kakao.com';
+  sender: 'test3@test.com';
+  receiver: '한민@kakao.com';
+  lastMessage: 'ㅂㅈㄷ';
+  lastMessageTime: '2025-04-08T12:43:51';
+  unreadCount: 5;
+  projectId: 34;
+  otherUserName: '한민';
+  otherUserProfile: 'https://devcouse4-team16-bucket.s3.ap-northeast-2.amazonaws.com/projects/1/project_image.jpg';
+  otherUserId: 12;
+}
+
+export type GetRoomsResponse = RoomType[];
+export default async function getRooms() {
+  const cookieStore = await cookies();
+  const token = cookieStore.get('accessToken')?.value;
+
+  if (!token) {
+    throw new Error('AccessToken이 없습니다.');
+  }
+  console.log('token', token);
+  // 사용자 이메일을 함께 보내야 하는데...
+  const email = decodeToken(token)?.username;
+  const response = await APIBuilder.get(`/chatrooms/rooms?member=${email}`)
+    .headers({
+      Cookie: `accessToken=${token}`,
+    })
+    .withCredentials(true)
+    .build()
+    .call<GetRoomsResponse>();
+
+  console.log('채팅방 목록', response.data);
+  return response.data;
+}
---
+++ src/utils/decodeToken.ts
@@ -0,0 +1,17 @@
+export function decodeToken(token: string): Record<string, any> | null {
+  try {
+    // 토큰은 "header.payload.signature" 형식으로 구성됨
+    const payload = token.split('.')[1]; // payload 부분 추출
+    if (!payload) {
+      throw new Error('토큰 포맷이 올바르지 않습니다.');
+    }
+
+    // Base64 디코딩 후 JSON 파싱
+    const decodedPayload = JSON.parse(atob(payload));
+    console.log('디코딩된 페이로드:', decodedPayload);
+    return decodedPayload;
+  } catch (error) {
+    console.error('토큰 디코딩에 실패했습니다.', error);
+    return null;
+  }
+}
