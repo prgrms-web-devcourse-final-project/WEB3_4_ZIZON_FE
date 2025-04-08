42dd5f03dcdfbda701ed6fc706a3b1aed308fd15
Sean <joajoa70584@gmail.com>
2025-04-08 15:42:08 +0900

    refactor: 이미지 업로드 API 추가 (#94)
    - S3에 이미지를 업로드하기 위한 postImageUpload 및 putS3Upload 함수 추가
    - presigned URL을 통해 이미지 파일을 S3에 업로드하는 로직 구현

---
+++ src/apis/imageUpload/postImageUpload.ts
@@ -0,0 +1,20 @@
+import { APIBuilder } from '@/utils/APIBuilder';
+
+interface S3Response {
+  presignedUrl: string;
+  accessUrl: string;
+}
+
+type TableUnionType = 'projects' | 'members' | 'products' | 'portfolios' | 'chating';
+
+export interface ImageUploadRequest {
+  folder: TableUnionType;
+  fileName: string;
+  contentType: string;
+}
+
+export const postImageUpload = async (request: ImageUploadRequest): Promise<S3Response> => {
+  const response = await APIBuilder.post('/api/s3/upload-url', request).build().call<S3Response>();
+
+  return response.data;
+};
