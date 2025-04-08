d969544f787e97aa56f536d8e794f6ff174cf657
Sean <joajoa70584@gmail.com>
2025-04-08 14:12:39 +0900

    style: 버튼 컴포넌트 스타일 개선 및 텍스트 수정 (#94)
    - ExpertStepFooterButton 컴포넌트에서 버튼에 cursor-pointer 클래스를 추가하여 클릭 가능성을 명확히 함
    - LabelWithIconButton 컴포넌트의 '취미 생활' 텍스트를 '취미생활'로 수정하여 일관성 강화

--- src/components/atoms/buttons/expertStepFooterButton/ExpertStepFooterButton.tsx
+++ src/components/atoms/buttons/expertStepFooterButton/ExpertStepFooterButton.tsx
@@ -16,14 +16,14 @@ function ExpertStepFooterButton({
   return (
     <div className="max-w-fit">
       <button
-        className="py-12 px-28 bg-black1 rounded-lg border border-black3 text-16 font-semibold text-black7 mr-16"
+        className="py-12 px-28 bg-black1 rounded-lg border border-black3 text-16 font-semibold text-black7 mr-16 cursor-pointer"
         onClick={onClickBefore}
       >
         이전
       </button>
       {state === 'next' ? (
         <button
-          className={`py-12 px-28 rounded-lg border text-16 font-semibold text-black1 ${
+          className={`py-12 px-28 rounded-lg border text-16 font-semibold text-black1 cursor-pointer ${
             disabled ? 'bg-black3 border-black3 cursor-not-allowed' : 'bg-black12 border-black12'
           }`}
           onClick={onClickNext}
@@ -33,7 +33,7 @@ function ExpertStepFooterButton({
         </button>
       ) : (
         <button
-          className={`py-12 px-28 rounded-lg border text-16 font-semibold text-black1 ${
+          className={`py-12 px-28 rounded-lg border text-16 font-semibold text-black1 cursor-pointer ${
             disabled ? 'bg-black3 border-black3 cursor-not-allowed' : 'bg-primary5 border-primary5'
           }`}
           onClick={onClickRegister}
