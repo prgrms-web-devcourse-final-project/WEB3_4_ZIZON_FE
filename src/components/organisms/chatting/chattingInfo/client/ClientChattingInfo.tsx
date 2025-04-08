b73fcc00fd0cc2de0d8c3936d5d048f3289c10c9
jjeongsu <jeongsu.dev@gmail.com>
2025-04-08 16:51:36 +0900

    feat : 채팅방 오른쪽 전문가 프로필 api 연동 및 구현(#97)

--- src/components/organisms/chatting/chattingInfo/client/ClientChattingInfo.tsx
+++ src/components/organisms/chatting/chattingInfo/client/ClientChattingInfo.tsx
@@ -1,21 +1,27 @@
+'use client';
+import { ExpertType, getExpert } from '@/apis/expert/getExpert';
+import getOfferList, { OfferListResponseType } from '@/apis/offer/getOfferList';
 import ChattingButtonGroup from '@/components/molecules/chat/ChattingButtonGroup';
 import ChattingExpertInfo from '@/components/molecules/chat/ChattingExpertInfo';
 import OfferInfo from '@/components/molecules/chat/OfferInfo';
-import { ExpertInfoType } from '@/types/expert';
-import { OfferInfoType } from '@/types/offer';
+
+interface ClientChattingInfoProps {
+  expertData: ExpertType | null;
+  offerListData: OfferListResponseType | null;
+  expertId: number | null;
+}
 
 export default function ClientChattingInfo({
-  offerInfo,
-  expertInfo,
-}: {
-  offerInfo: OfferInfoType;
-  expertInfo: ExpertInfoType;
-}) {
+  expertData,
+  offerListData,
+  expertId,
+}: ClientChattingInfoProps) {
+  const recentOffer = offerListData?.filter(offer => offer.expert_id === expertId).at(-1); // 해당 전문가의 offer를 찾음
   return (
     <div className="w-full max-w-402 flex flex-col gap-16">
-      <ChattingButtonGroup offerInfo={offerInfo} expertInfo={expertInfo} />
-      <OfferInfo offerInfo={offerInfo} type="client" />
-      <ChattingExpertInfo />
+      {/* <ChattingButtonGroup offerInfo={recentOffer} expertInfo={expertData} />
+      <OfferInfo offerInfo={recentOffer} type="client" /> */}
+      <ChattingExpertInfo expertData={expertData} />
     </div>
   );
 }
