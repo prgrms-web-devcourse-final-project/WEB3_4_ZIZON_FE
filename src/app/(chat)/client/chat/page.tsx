dfaddb5908f89c22a9fdb17a20fac0dfbc897820
jjeongsu <jeongsu.dev@gmail.com>
2025-04-09 16:19:04 +0900

    feat: 더미데이터 제거, Suspense 추가
    # Conflicts:
    #	src/app/(payment)/payments/page.tsx

--- src/app/(chat)/client/chat/page.tsx
+++ src/app/(chat)/client/chat/page.tsx
@@ -1,12 +1,15 @@
 import getRooms, { GetRoomsResponse } from '@/apis/chat/getRooms';
 import ChattingTemplate from '@/components/templates/chatTemplate/ChattingTemplate';
+import { Suspense } from 'react';
 
 export default async function ClientChatPage() {
   const chatRoomList: GetRoomsResponse = await getRooms();
-
+  console.log('채팅방목록!!', chatRoomList);
   return (
     <div className="w-full flex justify-center">
-      <ChattingTemplate chatRoomList={chatRoomList} />
+      <Suspense fallback="펄백로딩중">
+        <ChattingTemplate chatRoomList={chatRoomList} />
+      </Suspense>
     </div>
   );
 }
--- src/app/(payment)/payments/page.tsx
+++ src/app/(payment)/payments/page.tsx
@@ -2,8 +2,8 @@ import { postPayment } from '@/apis/payment/postPayment';
 import OrderTemplate from '@/components/templates/orderTemplate/OrderTemplate';
 
 export default async function OrderPage({
-                                          searchParams,
-                                        }: {
+  searchParams,
+}: {
   searchParams: Promise<{ id: string; type: string }>;
 }) {
   // 테스트 경로 : /payments?id=53&type=PROJECT
