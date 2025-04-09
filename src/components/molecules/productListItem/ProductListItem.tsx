38486a829c7d0dcdcdd468a11ca009325575b8b7
jjeongsu <jeongsu.dev@gmail.com>
2025-04-10 00:20:49 +0900

    feat : 스토어 상품 목록 조회 api 연결(#107)

--- src/components/molecules/productListItem/ProductListItem.tsx
+++ src/components/molecules/productListItem/ProductListItem.tsx
@@ -1,14 +1,11 @@
 import Image from 'next/image';
 import NumberReadability from '@/components/atoms/texts/numberReadability/NumberReadability';
 import SmallTag from '@/components/atoms/tags/smallTag/SmallTag';
+import { Product } from '@/apis/store/getProductList';
 
 interface ProductListItemProps {
   size: 'small' | 'large';
-  imageUrl: string;
-  category: '디지털 컨텐츠' | '리빙';
-  title: string;
-  seller_name: string;
-  price: number;
+  product: Product;
 }
 
 const SIZE_CONFIG = {
@@ -24,22 +21,15 @@ const SIZE_CONFIG = {
   },
 } as const;
 
-function ProductListItem({
-  size,
-  imageUrl,
-  category,
-  title,
-  seller_name,
-  price,
-}: ProductListItemProps) {
+function ProductListItem({ size, product }: ProductListItemProps) {
   const sizeStyle = SIZE_CONFIG[size];
 
   return (
     <div className={`${sizeStyle.container} flex flex-col gap-16 group`}>
       <div className={`${sizeStyle.imageStlye} rounded-2xl overflow-hidden`}>
         <Image
-          src={imageUrl || '/images/DefaultImage.png'}
-          alt={title}
+          src={product.thumbnailUrl || '/images/DefaultImage.png'}
+          alt={product.name}
           width={sizeStyle.imageSize}
           height={sizeStyle.imageSize}
           className="size-full object-cover group-hover:scale-105 transition-all duration-300 "
@@ -48,15 +38,18 @@ function ProductListItem({
 
       <div className="flex flex-col gap-8">
         <div className="flex gap-8">
-          <SmallTag text={category} theme={category === '리빙' ? 'lightOrange' : 'lightBlue'} />
+          <SmallTag
+            text={product.categoryName}
+            theme={product.categoryName === '리빙' ? 'lightOrange' : 'lightBlue'}
+          />
           <SmallTag text="소분류" theme="grey" />
         </div>
-        <span className="text-16 font-regular text-black12 truncate">{title}</span>
-        <span className="text-13 font-regular text-black7">{seller_name}</span>
+        <span className="text-16 font-regular text-black12 truncate">{product.name}</span>
+        <span className="text-13 font-regular text-black7">{product.expertName}</span>
       </div>
 
       <div className="flex text-20 font-bold text-black12">
-        <NumberReadability value={price} />원
+        <NumberReadability value={product.price} />원
       </div>
     </div>
   );
