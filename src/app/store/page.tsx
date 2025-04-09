61dd4f192927e032f54dfe496628ef922517f76d
jjeongsu <jeongsu.dev@gmail.com>
2025-04-10 00:20:49 +0900

    feat : 스토어 상품 목록 조회 api 연결(#107)

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,64 +1,23 @@
+import getProductList from '@/apis/store/getProductList';
 import StoreMainTemplate from '@/components/templates/store/storeMain/StoreMainTemplate';
-import { Suspense } from 'react';
 
-export interface ProductType {
-  category: '디지털 컨텐츠' | '리빙';
-  imageUrl: string;
-  price: number;
-  seller_name: string;
-  size: string;
-  title: string;
-}
-
-export const DUMMY_PRODUCT_LIST: ProductType[] = [
-  {
-    category: '디지털 컨텐츠',
-    imageUrl: '/images/DefaultImage.png',
-    price: 35000,
-    seller_name: '코딩천재',
-    size: 'large',
-    title: '디테일이 살아있는 차트 UI',
-  },
-  {
-    category: '디지털 컨텐츠',
-    imageUrl: '/images/DefaultImage.png',
-    price: 50000,
-    seller_name: '디자인마스터',
-    size: 'medium',
-    title: '모던한 웹사이트 디자인 템플릿',
-  },
-  {
-    category: '디지털 컨텐츠',
-    imageUrl: '/images/DefaultImage.png',
-    price: 15000,
-    seller_name: '개발왕',
-    size: 'small',
-    title: '효율적인 코드 스니펫 모음',
-  },
-  {
-    category: '리빙',
-    imageUrl: '/images/DefaultImage.png',
-    price: 20000,
-    seller_name: '교육전문가',
-    size: 'large',
-    title: 'React 초급 강의 자료',
-  },
-  {
-    category: '디지털 컨텐츠',
-    imageUrl: '/images/DefaultImage.png',
-    price: 45000,
-    seller_name: 'UI/UX 디자이너',
-    size: 'medium',
-    title: '사용자 친화적인 UI 컴포넌트',
-  },
-];
-
-export default function StorePage() {
+export default async function StorePage({
+  searchParams,
+}: {
+  searchParams: Promise<{ categoryId: string; page: string }>;
+}) {
+  const { categoryId, page } = await searchParams;
+  const {
+    products: productListData,
+    currentPage,
+    pageSize,
+    hasNext,
+  } = await getProductList({
+    page: Number(page) || 0,
+  });
   return (
     <div className="mt-72 flex justify-center">
-      <Suspense>
-        <StoreMainTemplate productList={DUMMY_PRODUCT_LIST} />
-      </Suspense>
+      <StoreMainTemplate productList={productListData} />
     </div>
   );
 }
--- src/components/templates/store/storeMain/StoreMainTemplate.tsx
+++ src/components/templates/store/storeMain/StoreMainTemplate.tsx
@@ -1,11 +1,13 @@
-import { ProductType } from '@/app/store/page';
+'use client';
+
+import { Product } from '@/apis/store/getProductList';
 import StoreMainContent from '@/components/organisms/store/StoreMainContent';
 import StoreSearchRegister from '@/components/organisms/store/StoreSearchResgister';
 
 // page로 부터 받을 데이터  : 상품 목록
-export default function StoreMainTemplate({ productList }: { productList: ProductType[] }) {
+export default function StoreMainTemplate({ productList }: { productList: Product[] }) {
   return (
-    <div className="w-full max-w-1280">
+    <div className="w-full max-w-1280 mb-200">
       <h1 className="font-semibold text-32 text-black12 mb-40">스토어</h1>
       <div className="w-full flex flex-col gap-32">
         <StoreSearchRegister />
