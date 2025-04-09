bc79e1a4e21e07b5e1e17a38fe3619e204346368
jjeongsu <jeongsu.dev@gmail.com>
2025-04-09 22:43:15 +0900

    feat: 상품목록 api 호출 함수 작성(#107)

--- src/apis/store/getProductList.ts
+++ src/apis/store/getProductList.ts
@@ -1,7 +1,17 @@
 import { APIBuilder } from '@/utils/APIBuilder';
+import { cookies } from 'next/headers';
 
-interface ProductListItem {
-  expert_id: number;
+export interface ProductListRequestType {
+  categoryId: number;
+  pageable: {
+    page: number;
+    size: number;
+    sort: string[];
+  };
+}
+
+export interface Product {
+  expert_id: number; // expertName
   category_id: number;
   title: string;
   stock: number;
@@ -9,10 +19,23 @@ interface ProductListItem {
   thumbnail_image: string;
 }
 
-type ProductListResponseType = Array<ProductListItem>;
+type ProductListResponseType = Array<Product>;
 
-export default async function getProductList() {
-  const response = await APIBuilder.get('/product')
+export default async function getProductList({
+  categoryId,
+  pageable,
+}: ProductListRequestType): Promise<ProductListResponseType> {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  if (!accessToken) {
+    throw new Error('Access token not found');
+  }
+  const query = encodeURIComponent(JSON.stringify(pageable));
+  const response = await APIBuilder.get(`/products?pageable=${query}&categoryId=${categoryId}`)
+    .headers({
+      Cookie: `access_token=${accessToken}`,
+      'Content-Type': 'application/json',
+    })
     .timeout(10000)
     .withCredentials(true)
     .build()
