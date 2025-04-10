38d033a99c8a12c971f31e7bdc924ef8df4b6343
Sean <joajoa70584@gmail.com>
2025-04-10 14:25:30 +0900

    feat: 내가 쓴 리뷰 API 응답 형식 개선 및 페이지네이션 로직 수정 (#114)
    - getMyReviews 함수의 반환 타입을 Review[]에서 GetMyReviewsResponse로 변경하여 페이지네이션 정보 포함
    - MyReviewPage 컴포넌트에서 페이지네이션 로직을 hasNext 속성을 기반으로 수정하여 다음 페이지 처리 개선
    - 프로젝트 정보를 비동기적으로 가져오는 로직 추가 및 리뷰 항목에 프로젝트 제목 표시 기능 구현

--- src/apis/review/getMyReviews.ts
+++ src/apis/review/getMyReviews.ts
@@ -7,17 +7,24 @@ interface GetMyReviewsParams extends HTTPParams {
   size?: number;
 }
 
+interface GetMyReviewsResponse {
+  reviews: Review[];
+  currentPage: number;
+  size: number;
+  hasNext: boolean;
+}
+
 /**
  * 내가 쓴 리뷰 목록을 가져옵니다.
  * @param params 페이지네이션 파라미터
- * @returns 내가 쓴 리뷰 목록
+ * @returns 내가 쓴 리뷰 목록 응답
  */
-export const getMyReviews = async (params?: GetMyReviewsParams): Promise<Review[]> => {
+export const getMyReviews = async (params?: GetMyReviewsParams): Promise<GetMyReviewsResponse> => {
   try {
     const response = await APIBuilder.get('/reviews/clients/me')
       .params(params)
       .build()
-      .call<Review[]>();
+      .call<GetMyReviewsResponse>();
 
     return response.data;
   } catch (error) {
