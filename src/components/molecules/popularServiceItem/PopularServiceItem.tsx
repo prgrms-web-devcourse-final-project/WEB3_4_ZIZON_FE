1253467caa8367daf778e8d1c7f4c8b812ce428d
Sean <joajoa70584@gmail.com>
2025-04-10 12:21:53 +0900

    feat: 서비스 카테고리 이미지 추가 및 인기 서비스 항목 업데이트 (#122)
    - 새로운 서비스 카테고리 이미지 4개 추가
    - PopularServiceItem 컴포넌트에서 이미지 경로 및 사용자 수 업데이트
    - 이미지 스타일 개선으로 사용자 경험 향상

--- src/components/molecules/popularServiceItem/PopularServiceItem.tsx
+++ src/components/molecules/popularServiceItem/PopularServiceItem.tsx
@@ -20,14 +20,16 @@ export default function PopularServiceItem({
 
   return (
     <Link href={linkTo} className="flex flex-col gap-12 items-start group">
-      <Image
-        src={imagePath}
-        alt="service-thumbnail-image"
-        width={302}
-        height={180}
-        className="rounded-2xl group-hover:shadow-style1 transition-all duration-300"
-        onError={() => setImagePath('/images/DefaultImage.png')}
-      />
+      <div className="w-302 h-180 rounded-2xl overflow-hidden">
+        <Image
+          src={imagePath}
+          alt="service-thumbnail-image"
+          width={302}
+          height={180}
+          className="size-full object-cover rounded-2xl transition-all duration-300 group-hover:scale-110"
+          onError={() => setImagePath('/images/DefaultImage.png')}
+        />
+      </div>
       <h4 className="font-semibold text-20 text-black10">{title}</h4>
       <span className="flex gap-4 items-center font-regular text-16 text-black7 object-contain">
         <NumberReadability value={numberOfUsers} />
